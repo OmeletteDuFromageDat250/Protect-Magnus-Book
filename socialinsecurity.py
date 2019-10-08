503dc78f46a33d7490383c646991edbfd690942e
k506 <salvato@emersion.fr>
2019-10-08 15:49:59 +0200

    limitation file size

--- socialinsecurity.py
+++ socialinsecurity.py
@@ -1,2 +1,4 @@
 # configured as the entry point of the app, simply imports app to start application, just run 'flask run' to start
 from app import app
+
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
